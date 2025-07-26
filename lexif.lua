82a48c35326c54c38bc22089c6ade2b4ec5d9db1
neosolaris <batutah@gmail.com>
2025-07-26 18:36:28 +0900

    update files

--- lexif.lua
+++ lexif.lua
@@ -1,33 +1,26 @@
 -- TITLE: Image Exif Info Manage Program
 -- DATE: 2025-07-18 00:13:29
 -- TODO: 
+-- - getopt 적용하기
+-- - metadata 만들고 이를 관리하기: 보류: 그림 파일마다 메타데이터가 다르다. csv 시 문제 발생
+-- - ImageDescription 위주로 관리하는 것이 바람직, 아니면 내가 필요한 요소 몇개만 테이블로 관리가 바람직
 
+local getopt = require'getopt'
 local m = require'lim'
 
---// run_command(command)
-local function run_command(command)
-	local handle = io.popen(command)
-	local result = handle:read("*a")
-	handle:close()
-	return result
-end
-
-local function show_help()
-	print("Usage: lexif.lua <option> <files>")
-	print("-h: help")
-	print("-l <files>: list exif info")
-	print("-s <keyword> <files>: search exif info")
-end
+local p = m.path(arg[0])
+local version = '0.1'
 
 local function list_info(optargs)
 	local cmd = ''
-	if #optargs == 0 then
-		cmd = 'exiftool .'
+	if next(optargs) then
+		cmd = 'exiftool ' .. m.catlist(optargs)
 	else
-		cmd = 'exiftool ' .. table.concat(optargs, ' ')
+		cmd = 'exiftool .'
 	end
-	local res = run_command(cmd)
-	print(res)
+	--print(cmd)
+	--local res = m.cmd(cmd)
+	m.printl(m.cmd(cmd))
 end
 
 local function search_info(optargs)
@@ -38,10 +31,10 @@ local function search_info(optargs)
 	end
 
 	if #optargs == 1 then
-		local files = assert(io.popen("find . -type f -name '*.jpg'"))
+		local files = list_imgfiles({'.'})
 		for file in files:lines() do
 			cmd = 'exiftool ' ..file.. ' | grep -i ' .. optargs[1]
-			local result = run_command(cmd)
+			local result = m.cmd(cmd)
 			if result and result:match("^%s*$") == nil then
 				print('==>',file)
 				print(result)
@@ -49,11 +42,96 @@ local function search_info(optargs)
 		end
 	else
 		cmd = 'exiftool ' ..optargs[2].. ' | grep -i ' .. optargs[1]
-		local result = run_command(cmd)
+		local result = m.cmd(cmd)
 		print(result)
 	end
 end
 
+local function get_imgdesc(optargs)
+	local cmd_pre = 'exiftool -s -s -s -ImageDescription'
+	local cmd_last = '.'
+
+	if next(optargs) then
+		cmd_last = m.catlist(optargs)
+	end
+
+	local cmd = m.strf("%s %s", cmd_pre, cmd_last)
+	--print('--> get_imgdesc:', cmd)
+	--print(cmd)
+	local result = m.cmd(cmd)
+	return result
+end
+
+local function show_imgdesc(optargs)
+	m.printl(get_imgdesc(optargs))
+end
+
+local function cid(file)
+	if not file then return false end
+	-- show input
+	local result = get_imgdesc({file})
+	m.printf("%s: %s\n", file, m.catlist(result))
+	local input = m.read('ImageDescription: ')
+	-- get input
+	local cmd = m.strf("exiftool -overwrite_original -ImageDescription='%s' %s", input, file)
+	m.printl(m.cmd(cmd))
+end
+
+local function change_imgdesc(optargs)
+	if #optargs == 0 then
+		local files = list_imgfiles(optargs)
+		for file in m.iterlist(files) do
+			cid(file)
+		end
+	elseif #optargs == 1 then
+		if m.isdir(optargs[1]) then
+			path = optargs[1]
+			local files = m.lfs(path)
+			for _,file in ipairs(files) do
+				cid(file)
+			end
+		elseif m.isfile(optargs[1]) then
+			--print(optargs[1], 'is file!')
+			cid(optargs[1])
+		else
+			print('--> optargs is wrong!')
+			os.exit(1)
+		end
+	else
+		for file in m.each(optargs) do
+			cid(file)
+		end
+	end
+end
+
+local function create_metacsv(optargs)
+	local cmd = ''
+	if next(optargs) then
+		cmd = string.format('exiftool -csv -r %s > metadata.csv', m.catlist(optargs))
+	else
+		cmd = 'exiftool -csv -r . > metadata.csv'
+	end
+	print('-> create_metacsv:', cmd)
+	local result = m.cmd(cmd)
+	if result then print("--> create_metacsv(): metadata.csv created!") end
+end
+
+local function show_help()
+	local list = {
+	"----------------------------------------",
+	m.strf("%s v%s Brosu", p.name, version),
+	"----------------------------------------",
+	m.strf("Usage: %s <option> <files>", p.name),
+	"  -h: help",
+	"  -f <path>   list image files",
+	"  -l <files>  list exif info",
+	"  -i <files>  show ImageDescription info",
+	"  -I <files>  edit ImageDescription info",
+	"  -s <keyword> <files>  search exif info",
+	"  -m {img_dir} create metadata.csv from img_dir",
+}
+	m.printl(list)
+end
 
 -----------------------------------------------------------------
 -- # Main
@@ -66,8 +144,16 @@ if optname == '-l' then
 elseif optname == '-s' then
 	search_info(optargs)
 elseif optname == '-f' then
-	local res = m.lsf('.','*.jpg')
-	for _,f in ipairs(res) do print(f) end
+	local keys = {'*.jpg','*.png','*.tif'}
+	m.printl(m.lsf('.', keys))
+-- Get ImageDescription
+elseif optname == '-i' then
+	show_imgdesc(optargs)
+-- Put ImageDescription
+elseif optname == '-I' then
+	change_imgdesc(optargs)
+elseif optname == '-m' then
+	create_metacsv(optargs[1])
 else
 	show_help()
 end
